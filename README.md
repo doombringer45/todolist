35b24a5da3cf42c7268e98329e37ec0bc7acf402
changxiu1 <graham.jimenez95@gmail.com>
2017-02-23 19:47:01 +0800

    to do list

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # todolist
+
+
+to run the project in xcode, just go to platforms/ios/ then select the xcode project. 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,76 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $ionicModal, $window, $timeout) {
+
+$ionicModal.fromTemplateUrl('my-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+   
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+
+
+  $scope.taskItem = {item:""};
+
+  $scope.items = JSON.parse(window.localStorage.getItem("todoItem")) || [];
+
+
+
+  $scope.saveItem = function(item){
+
+    var a = $window.localStorage.getItem("todoItem") ;
+    a = JSON.parse(a) || [];
+      console.log(a);
+    a.push(item);
+    $window.localStorage.setItem("todoItem", JSON.stringify(a));
+   
+    $timeout(function(){
+        $scope.$apply(function( ){
+        $scope.items = a; 
+         $scope.taskItem = {item:""};
+    });
+
+    })
+  
+    $scope.modal.hide();
+
+  }
+
+  $scope.clear = function(){
+
+   
+    $timeout(function(){
+        $scope.$apply(function( ){
+        $scope.items = []; 
+        $window.localStorage.clear();
+    });
+
+    })
+  
+
+
+
+  }
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
